69acb0397b8479418186327c1a116df588cd257c
SSK137 <saurabhkodam@gmail.com>
2022-04-25 12:00:24 +0530

    EmployeeWage: Daily_WageMonthly UC5 Code

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -3,6 +3,8 @@ public class Employee_Wage {
 
     public static final int FULL_TIME = 2;
     public static final int PART_TIME = 1;
+    public static final int WORKING_DAYS = 20;
+    public static final int WAGE_PER_HR = 20;
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Problems");
 
@@ -10,23 +12,32 @@ public class Employee_Wage {
         Random rdm = new Random();           //rdm is veriable to store random number
         int present = rdm.nextInt(2);
         //Check if employee is present
+        int totalWage = 0;
         int WagePerHr = 20;
-        int dailyWage, workingHrs;
+        int dailyWage;
 
-        switch (present){
-            case FULL_TIME :
-                workingHrs = 8;
-                System.out.println("Employee Working Fullday");
-                break;
-            case PART_TIME:
-                workingHrs =4;
-                System.out.println("Employee Working Part Time");
-                break;
-            default:
-                workingHrs = 0;
-                System.out.println("Employee Absent");
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            present = rdm.nextInt(3);
+            int workingHrs = 0;
+
+            switch (present) {
+                case FULL_TIME:
+                    workingHrs = 8;
+                    System.out.println("Employee Working Fullday");
+                    break;
+                case PART_TIME:
+                    workingHrs = 4;
+                    System.out.println("Employee Working Part Time");
+                    break;
+                default:
+                    workingHrs = 0;
+                    System.out.println("Employee Absent");
+            }
+
+            dailyWage = workingHrs * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + dailyWage);
+            totalWage += dailyWage;
         }
-        dailyWage = workingHrs * WagePerHr;
-        System.out.println("Daily wage is " + dailyWage);
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
