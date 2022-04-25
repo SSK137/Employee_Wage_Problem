02c93fb7b35046104445fe4cf47a5bd74fe79b3e
SSK137 <saurabhkodam@gmail.com>
2022-04-25 11:03:33 +0530

    EmployeeWage: Attendance UC Code

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Problems");
+
+        Random rdm = new Random();           //rdm is veriable to store random number
+        int present = rdm.nextInt(2);
+        //Check if employee is present
+        if (present == 1)
+            System.out.println("Employee is present.");
+        else
+            System.out.println("Employee is absent.");
     }
 }
