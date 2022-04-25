6672d850ac8913d04dadfb30ffab24cf2c507451
SSK137 <saurabhkodam@gmail.com>
2022-04-25 11:46:00 +0530

    EmployeeWage: Daily_WageUsingSwitch UC4 Code

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,28 +1,32 @@
 import java.util.*;
 public class Employee_Wage {
+
+    public static final int FULL_TIME = 2;
+    public static final int PART_TIME = 1;
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Problems");
+
 
         Random rdm = new Random();           //rdm is veriable to store random number
         int present = rdm.nextInt(2);
         //Check if employee is present
         int WagePerHr = 20;
         int dailyWage, workingHrs;
-        if (present == 0) {
-            //Employee is absent
-            workingHrs = 0;
-            System.out.println("Employee is absent");
-        } else if (present == 1) {
-            //Employee working as part-time
-            workingHrs = 8;
-            System.out.println("Employee working as part-time");
-        } else {
-            //Employee working as full time
-            workingHrs = 16;
-            System.out.println("Employee working as full time");
+
+        switch (present){
+            case FULL_TIME :
+                workingHrs = 8;
+                System.out.println("Employee Working Fullday");
+                break;
+            case PART_TIME:
+                workingHrs =4;
+                System.out.println("Employee Working Part Time");
+                break;
+            default:
+                workingHrs = 0;
+                System.out.println("Employee Absent");
         }
         dailyWage = workingHrs * WagePerHr;
         System.out.println("Daily wage is " + dailyWage);
-
     }
 }
