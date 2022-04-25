2483641a03b29a6218e18a8008a3fbd55365e0e3
SSK137 <saurabhkodam@gmail.com>
2022-04-25 11:37:00 +0530

    EmployeeWage: Daily_WageParttime UC2 Code

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -6,12 +6,23 @@ public class Employee_Wage {
         Random rdm = new Random();           //rdm is veriable to store random number
         int present = rdm.nextInt(2);
         //Check if employee is present
-        int WagePerHr =20;
-        int wrkHrs = 8;
-        int dailyWage = present * WagePerHr * wrkHrs;
-        if (present == 1)
-            System.out.println("Employee is present."+dailyWage);
-        else
-            System.out.println("Employee is absent."+dailyWage);
+        int WagePerHr = 20;
+        int dailyWage, workingHrs;
+        if (present == 0) {
+            //Employee is absent
+            workingHrs = 0;
+            System.out.println("Employee is absent");
+        } else if (present == 1) {
+            //Employee working as part-time
+            workingHrs = 8;
+            System.out.println("Employee working as part-time");
+        } else {
+            //Employee working as full time
+            workingHrs = 16;
+            System.out.println("Employee working as full time");
+        }
+        dailyWage = workingHrs * WagePerHr;
+        System.out.println("Daily wage is " + dailyWage);
+
     }
 }
