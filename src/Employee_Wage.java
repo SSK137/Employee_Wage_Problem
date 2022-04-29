9d9a04ed5a9566a3931e501d86f0b7d0965a6373
SSK137 <saurabhkodam@gmail.com>
2022-04-29 13:27:53 +0530

    Employee Wage : Wage for multiple Company

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,56 +1,81 @@
 import java.util.*;
-public class Employee_Wage {
+public class Employee_Wage<Employee_Type> {
 
     public static final int FULL_TIME = 2;
     public static final int PART_TIME = 1;
-    public static final int MAX_WORKING_DAYS = 20;
-    public static final int WAGE_PER_HR = 20;
-    public static final int MAX_WORKING_HRS = 100;
-    public void Compute_Wage(String Companyname, int WagePerHR, int MAX_WORKING_Days, int MAX_WORKING_HRs){
+    public static int MAX_WORKING_DAYS = 20;
+    public static int WAGE_PER_HR = 20;
+    public static int MAX_WORKING_HRS = 100;
+    public static String Company_Name;
+    int TotalWage;
 
-        System.out.println("Details of " + Companyname + " employee");
-        System.out.println("Wage per hour:" + WagePerHR);
-        System.out.println("Maximum working days:" + MAX_WORKING_Days);
-        System.out.println("Maximum working hours:" + MAX_WORKING_HRs);
+    Employee_Wage(String CompanyName,int WagePerHR,int Max_WorkingDays,int Max_WorkingHrs){
 
-
-        Random rdm = new Random();           //rdm is veriable to store random number
-        //Check if employee is present
-        int totalWage = 0;
-        int dailyWage;
-        int workingHrs = 0;
-        int day = 1, totalWorkingHrs = 0;
-
-        for (day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
-            int present = rdm.nextInt(3);
-
-            switch (present) {
-                case FULL_TIME:
-                    workingHrs = 8;
-                    System.out.println("Employee Working Fullday");
-                    break;
-                case PART_TIME:
-                    workingHrs = 4;
-                    System.out.println("Employee Working Part Time");
-                    break;
-                default:
-                    workingHrs = 0;
-                    System.out.println("Employee Absent");
-                    break;
-            }
-
-            dailyWage = workingHrs * WAGE_PER_HR;
-            System.out.println("Day " + day + " workingHrs is " + workingHrs + " wage is: " + dailyWage);
-            totalWage += dailyWage;
-        }
-        System.out.println("Total wage for a month is " + totalWage);
+        Company_Name=CompanyName;
+        WAGE_PER_HR=WagePerHR;
+        MAX_WORKING_DAYS=Max_WorkingDays;
+        MAX_WORKING_HRS=Max_WorkingDays;
+        TotalWage=0;
     }
+
+    int EmployeeWorkType(){
+        return (int) (Math.random()*100) % 3;
+    }
+
+    int workingHRS(int Employee_Type){
+        return switch (Employee_Type) {
+            case FULL_TIME -> 8;
+            case PART_TIME -> 4;
+            default -> 0;
+        };
+    }
+
+    public void calculateTotalWage()
+    {
+        System.out.println("Computation of total wage of " + Company_Name + " employee");
+        int workingHrs;
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+        {
+            int Employee_Type = EmployeeWorkType();
+            workingHrs= workingHRS(Employee_Type);
+            int wage = workingHrs * WAGE_PER_HR;
+            TotalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs , workingHrs);
+        }
+
+    }
+
+    public String toString()
+    {
+        System.out.println("Details of " + Company_Name + " employee");
+        System.out.println("Wage per hour:" + WAGE_PER_HR);
+        System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+        System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+        return "Total wage for a month of " + Company_Name + " employee is " + TotalWage + "";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Problems");
-        Employee_Wage Emp_Wage = new Employee_Wage();
-        Emp_Wage.Compute_Wage("Amazon",20,20,100);
-        Emp_Wage.Compute_Wage("FlipKart",20,15,200);
+        System.out.println("Welcome");
+        /*
+        Creating Employee_Wage Class Objects and calling the CalculateTotalWage();
+         */
+        Employee_Wage amazon = new Employee_Wage("Amazon", 40, 15, 200 );
+
+        //Total Wage Using object amazon
+        amazon.calculateTotalWage();
+        System.out.println(amazon);
+
+        Employee_Wage zensar = new Employee_Wage("Zensar",20,10,100);
+        //Total Wage Using 3rd object zensar
+        zensar.calculateTotalWage();
+        System.out.println(zensar);
+
+
+        Employee_Wage IBM = new Employee_Wage("IBM", 20, 20, 100);
+        //Total Wage Using 3rd object IBM
+        IBM.calculateTotalWage();
+        System.out.println(IBM);
 
     }
 }
